0c693b6be1dbfd7d63c626c1dacc552e6c9713c4
bax082024 <bax082024@gmail.com>
2024-10-20 17:05:00 +0200

    structure setup

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+class CustomerSegmentationExample
+{
+  static void Main(string[] args)
+  {
+    
+  }
+}
