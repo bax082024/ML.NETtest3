2a8cc45966bdac4ab6f69cad2b915916a5010089
bax082024 <bax082024@gmail.com>
2024-10-20 17:14:55 +0200

    added pipeline

--- Program.cs
+++ Program.cs
@@ -6,6 +6,11 @@ class CustomerSegmentationExample
   static void Main(string[] args)
   {
     var mlContext = new MLContext();
+    
     IDataView dataView = mlContext.Data.LoadFromTextFile<CustomerData>("customerData.csv", hasHeader: true, separatorChar: ',');
+
+    var pipeline = mlContext.Transforms.Concatenate("Features", nameof(CustomerData.Age), nameof(CustomerData.Salary))
+        .Append(mlContext.Clustering.Trainers.KMeans(featureColumnName: "Features", numberOfClusters: 3));
+
   }
 }
